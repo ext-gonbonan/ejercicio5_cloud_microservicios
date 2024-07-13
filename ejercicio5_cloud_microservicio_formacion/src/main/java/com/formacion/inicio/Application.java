f53f8b7d0e012021e4f93e9cd2dca5873e2c4f1e
ext-gonbonan <antonio.gonzalez@altia.es>
2024-07-13 14:23:44 +0100

    Actualización

--- ejercicio5_cloud_microservicio_formacion/src/main/java/com/formacion/inicio/Application.java
+++ ejercicio5_cloud_microservicio_formacion/src/main/java/com/formacion/inicio/Application.java
@@ -2,6 +2,7 @@ package com.formacion.inicio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 
@@ -13,6 +14,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@LoadBalanced
 	@Bean
 	public RestClient restClient() {
 		return RestClient.create();
